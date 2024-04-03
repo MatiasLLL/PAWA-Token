a0d8ab12f5c8e8f566b4c12fd45f287ecdd0146f
MatiasLLL <matias.larralde97@gmail.com>
2024-04-03 11:07:25 -0700

    modification scripts deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,9 +3,9 @@ import { ethers } from "hardhat";
 async function main() {
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
-    const spaceDolphin = await ethers.deployContract("PAWA");
-    await spaceDolphin.waitForDeployment();
-    console.log("PAWA address:", await spaceDolphin.getAddress());
+    const pawa = await ethers.deployContract("PAWA");
+    await pawa.waitForDeployment();
+    console.log("PAWA address:", await pawa.getAddress());
 
 }
 
